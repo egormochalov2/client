a7f1b62762ba83a7a42aac7f5a370247cfcf4756
Ubludock1 <egor18mochalov@yandex.ru>
2022-06-21 18:29:42 +0300

    eeeeeEe12

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -9,8 +9,8 @@ import {fetchBrands, fetchDevice, fetchTypes} from "../http/deviceAPI";
 import Pages from "../components/Pages";
 import { Link } from 'react-router-dom';
 import tolst from '../assets/tolst.jpg'
-import chex from '../assets/chex.jpg'
-import koft from '../assets/koft.jpg'
+import chex from '../assets/koft.jpg'
+import koft from '../assets/koft.jpeg'
 
 const Shop = observer(() => {
     const {device} = useContext(Context);
